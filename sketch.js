7880da72a26e827ec460d353a5c514b76c88748e
EduardoBarbara12 <00001105489164SP@al.educacao.sp.gov.br>
2024-05-23 15:07:07 -0300

    atualizando códigos

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//variaveis bola
+let yBola = 230;
+let xBola = 295;
+let diametro= 26;
+let raio = diametro / 2;
+
+//vaiaveis velocidade bola
+let velozXBola = 6;
+let velozYBola = 4;
+
+//raquete nossa
+let xRaquete = 5
+let yRaquete = 220
+let larguraRaquete = 10
+let alturaRaquete = 90
+
+//raquete inimigo
+let xRaqueteOponente = 585
+let yRaqueteOponente = 220
+let velocidadeYOponente;
+
+//pontos
+let Pontos = 0;
+let oponentePontos = 0;
+
+//colisão
